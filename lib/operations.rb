fe7ceb1c287d63197d566c8cc175a5c386005815
Becky Zorn <bczorn@gmail.com>
2019-08-21 02:30:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
-    "unsafe"
+    true
   else
-    "safe"
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-	(40 < speed < 60) ? "safe" : "unsafe"
+	(40 < speed < 60) ? false : true
 end
 	
 
